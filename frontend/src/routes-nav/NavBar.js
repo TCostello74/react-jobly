b709656ad69d7935df1f510a641a3255cde0332c
tcostello74 <tcostello74@yahoo.com>
2023-11-15 13:32:49 -0500

    Nav-bar

---
+++ frontend/src/routes-nav/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/companies">Companies</Link>
+      <Link to="/jobs">Jobs</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/signup">Signup</Link>
+      <Link to="/profile">Profile</Link>
+      {/* Add more navigation links as needed */}
+    </nav>
+  );
+}
+
+export default NavBar;
